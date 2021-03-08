e59415c69653b95607224bdfcd338d3c49e6d0cc
tiktakisrael <lifeitk@gmail.com>
2021-03-09 00:52:41 +0200

    padding main div

--- src/components/article/secondPage/createArticle.js
+++ src/components/article/secondPage/createArticle.js
@@ -6,6 +6,7 @@ const Div = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    padding: 30px 0px 0px 0px;
 `
 const Input = styled.input.attrs({multiline:true})`
     width: 500px;
